cdcfcd48383f40b4b5d63264ff40aff75b742ffb
Anders Sundin <olsu2201@student.miun.se>
2024-02-02 08:49:34 +0100

    Mergeing from extra branch

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BooksApp.Data;
+using BooksApp.Models;
+
+namespace BooksApp.Controllers
+{
+    public class LoanController : Controller
+    {
+        private readonly BookContext _context;
+
+        public LoanController(BookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Loan
+
+
+
+        public async Task<IActionResult> Index(string? searchParam)
+        {
+            if (searchParam != null)
+            {
+                string searchString = searchParam;
+                IEnumerable<BookModel> bookTitle = await _context.Books
+                 .Where(b => EF.Functions.Like(b.Title, $"%{searchString}%"))
+                 .Include(c => c.Author)
+                .ToListAsync();
+
+
+                return View(bookTitle);
+            }
+
+            var bookContext = _context.Books.Include(b => b.Author);
+
+            return View(await bookContext.ToListAsync());
+        }
+
+        // GET: Loan/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookModel = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bookModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookModel);
+        }
+
+        // GET: Loan/Create
+        public IActionResult Create()
+        {
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Loan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseYear,Pages,Available,AuthorId,UserId,LoanDate")] BookModel bookModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", bookModel.AuthorId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", bookModel.UserId);
+            return View(bookModel);
+        }
+
+        // GET: Loan/Edit/5
+        public async Task<IActionResult> NewLoan(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookModel = await _context.Books.FindAsync(id);
+            if (bookModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Name", bookModel.AuthorId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Name", bookModel.UserId);
+            return View(bookModel);
+        }
+
+        // POST: Loan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NewLoan(int id, [Bind("Id,Title,ReleaseYear,Pages,Available,AuthorId,UserId,LoanDate")] BookModel bookModel)
+        {
+            if (id != bookModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookModelExists(bookModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", bookModel.AuthorId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", bookModel.UserId);
+            return View(bookModel);
+        }
+
+        // GET: Loan/Edit/5
+        public async Task<IActionResult> ReturnBook(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookModel = await _context.Books.FindAsync(id);
+            if (bookModel == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(bookModel);
+        }
+
+        // POST: Loan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AcceptReturn (int? id)
+        {
+              if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookModel = await _context.Books.FindAsync(id);
+            if (bookModel == null)
+            {
+                return NotFound();
+            }
+
+            // Sätt fälten till null
+            bookModel.UserId = null;
+            bookModel.LoanDate = null;
+            bookModel.Available = true;
+
+            // Spara ändringarna i databasen
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Loan/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookModel = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bookModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookModel);
+        }
+
+        // POST: Loan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bookModel = await _context.Books.FindAsync(id);
+            if (bookModel != null)
+            {
+                _context.Books.Remove(bookModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookModelExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Migrations/20240201122203_fourth.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class fourth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_UserModel_UserId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserModel",
+                table: "UserModel");
+
+            migrationBuilder.RenameTable(
+                name: "UserModel",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Users_UserId",
+                table: "Books",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Users_UserId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "UserModel");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserModel",
+                table: "UserModel",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_UserModel_UserId",
+                table: "Books",
+                column: "UserId",
+                principalTable: "UserModel",
+                principalColumn: "Id");
+        }
+    }
+}
